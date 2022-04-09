0a8e8575680a74801ee408d94f1d9098efccf258
sergio Shaon <shaon463@gmail.com>
2022-04-09 15:41:05 +0600

    Initial Commit

---
+++ InfinityCourse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfinityCourse
+//
+//  Created by sergio shaon on 9/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InfinityCourse/InfinityCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinityCourseApp.swift
+//  InfinityCourse
+//
+//  Created by sergio shaon on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct InfinityCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
