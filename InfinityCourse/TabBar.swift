caeccb71ef9b0e7c488fe8e3267b0c2003b6a64c
sergio Shaon <shaon463@gmail.com>
2022-04-14 17:01:33 +0600

    frame modifier usage

--- InfinityCourse/TabBar.swift
+++ InfinityCourse/TabBar.swift
@@ -21,7 +21,9 @@ struct TabBar: View {
             HStack {
                 ForEach(tabItems) { item in
                     Button {
-                        selectedTab = item.tab
+                        withAnimation {
+                            selectedTab = item.tab
+                        }
                     }label:{
                         VStack(spacing: 0) {
                             Image(systemName: item.icon)
@@ -41,6 +43,44 @@ struct TabBar: View {
             .padding(.top, 14)
             .frame(height: 88, alignment: .top)
             .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+            .background(
+                HStack{
+                    if selectedTab == .library {Spacer()}
+                    if selectedTab == .explore {Spacer()}
+                    if selectedTab == .notification {
+                        Spacer()
+                        Spacer()}
+                    Circle().fill(.pink).frame(width: 85)
+                    if selectedTab == .home {Spacer()}
+                    if selectedTab == .explore {
+                        Spacer()
+                        Spacer()}
+                    if selectedTab == .notification {Spacer()}
+                    
+                }
+                    .padding()
+            )
+            .overlay(
+                    HStack{
+                        if selectedTab == .library {Spacer()}
+                        if selectedTab == .explore {Spacer()}
+                        if selectedTab == .notification {
+                            Spacer()
+                            Spacer()}
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.pink)
+                            .frame(width: 30, height: 5)
+                            .frame(width: 75)
+                            .frame(maxHeight: .infinity, alignment: .top)
+                        if selectedTab == .home {Spacer()}
+                        if selectedTab == .explore {
+                            Spacer()
+                            Spacer()}
+                        if selectedTab == .notification {Spacer()}
+                        
+                    }
+                        .padding(.horizontal, 15)
+            )
             .frame(maxHeight: .infinity, alignment: .bottom)
             .ignoresSafeArea()
         }
